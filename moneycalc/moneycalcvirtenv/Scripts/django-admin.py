d61c9408a93a61b41adffed4e90afcbe7fd81ee7
echemes <echemes@ECHEMESNB>
2017-01-16 14:42:38 +0200

    Add project files.

---
+++ moneycalc/moneycalcvirtenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\echemes\Documents\Visual Studio 2015\Projects\DjangoAppTest\moneycalc\moneycalcvirtenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
